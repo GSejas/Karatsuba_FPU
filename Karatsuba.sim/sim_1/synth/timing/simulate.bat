59965e9ba80048362c88128e073356a73fe7c35f
Jsequeira <jsequeira03@gmail.com>
2016-09-07 23:59:49 -0600

    Big structure changes, this in turn will make DC synthesis and Vivado synthesis easier. Hopefully

--- Karatsuba.sim/sim_1/synth/timing/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2016.2\\bin
-call %xv_path%/xsim testbench_RKOA_time_synth -key {Post-Synthesis:sim_1:Timing:testbench_RKOA} -tclbatch testbench_RKOA.tcl -view C:/Users/jsequeira/Proyectos/testbench_RKOA_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
