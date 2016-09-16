51d1bdf6b6fa4496e8852728a72b96497a5df93d
Jsequeira <jsequeira03@gmail.com>
2016-09-16 10:04:28 -0600

    asda

---
+++ Cordic/CORDIC_FUNCIONAL_Viv/CORDIC_FUNCIONAL_Viv.srcs/sim_1/new/testbench_CORDICArch2.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/13/2016 09:26:31 AM
+// Design Name: 
+// Module Name: testbench_CORDICArch2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module testbench_CORDICArch2(
+
+    );
+endmodule
